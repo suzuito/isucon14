70740d47a4d0d7429aa98e95c06f32f15b2d7187
Taito Suzuki <2217408+suzuito@users.noreply.github.com>
2024-12-08 13:11:09 +0900

    RetryAfterMsを2500にする

--- home/isucon/webapp/go/chair_handlers.go
+++ home/isucon/webapp/go/chair_handlers.go
@@ -198,10 +198,11 @@ func chairGetNotification(w http.ResponseWriter, r *http.Request) {
 	yetSentRideStatus := RideStatus{}
 	status := ""
 
+	// TODO chair_idにインデックスを貼る。
 	if err := tx.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id = ? ORDER BY updated_at DESC LIMIT 1`, chair.ID); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			writeJSON(w, http.StatusOK, &chairGetNotificationResponse{
-				RetryAfterMs: 30,
+				RetryAfterMs: 2500,
 			})
 			return
 		}
@@ -261,7 +262,7 @@ func chairGetNotification(w http.ResponseWriter, r *http.Request) {
 			},
 			Status: status,
 		},
-		RetryAfterMs: 30,
+		RetryAfterMs: 2500,
 	})
 }
 
