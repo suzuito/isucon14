16459ce77a1a16a5e6c6c6bbf5c8f11014731a0f
Taito Suzuki <2217408+suzuito@users.noreply.github.com>
2024-12-08 13:12:37 +0900

    save

--- home/isucon/webapp/go/chair_handlers.go
+++ home/isucon/webapp/go/chair_handlers.go
@@ -198,7 +198,6 @@ func chairGetNotification(w http.ResponseWriter, r *http.Request) {
 	yetSentRideStatus := RideStatus{}
 	status := ""
 
-	// TODO chair_idにインデックスを貼る。
 	if err := tx.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id = ? ORDER BY updated_at DESC LIMIT 1`, chair.ID); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			writeJSON(w, http.StatusOK, &chairGetNotificationResponse{
